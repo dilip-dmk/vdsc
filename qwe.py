20a4d46f5d5028d68e5e460bc24d8e30807f95a5
dilip-dmk <dilip_dmk@yahoo.com>
2016-12-08 20:05:15 +0100

    Tokenisation

--- qwe.py
+++ qwe.py
@@ -1,7 +1,30 @@
 import os
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
+from nltk import word_tokenize
+import re
+
 
 corpusdir = 'python/' # Directory of corpus.
 
 newcorpus = PlaintextCorpusReader(corpusdir, '.*')
-print(newcorpus.words())
+print(newcorpus.fileids()[0])
+print(type(newcorpus))
+#print newcorpus.raw()
+print newcorpus.words(newcorpus.fileids()[0])
+print(len(newcorpus.words()))
+
+tokens = word_tokenize(newcorpus.raw())
+#type(tokens)
+print len(tokens)
+print tokens[:50]
+#tokens[:10]
+print newcorpus.sents()
+print
+
+#to remove comments
+def removeComments(string):
+    string = re.sub(re.compile("/\*.*?\*/",re.DOTALL ) ,"" ,string) # remove all occurance streamed comments (/*COMMENT */) from string fdf
+    string = re.sub(re.compile("//.*?\n" ) ,"" ,string) # remove all occurance singleline comments (//COMMENT\n ) from string
+    return string
+
+print(removeComments(newcorpus.words(newcorpus.raw())))
